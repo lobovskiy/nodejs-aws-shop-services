b8d289ed90496cda11eb510cf660e746588b0257
Artem Lobovskii <lobovskiy@yandex.ru>
2024-06-20 21:13:02 +0300

    feat: create dynamodb tables for products and stocks

--- services/products/lib/nested-stacks/base-stack.ts
+++ services/products/lib/nested-stacks/base-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
+import { TableV2, AttributeType } from 'aws-cdk-lib/aws-dynamodb';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { SERVICE_API_PATH } from '../../src/consts';
@@ -13,18 +14,41 @@ export class ProductsBaseStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    this.getProductsList = new NodejsFunction(this, 'LambdaGetProductsList ', {
+    const productsTable = new TableV2(this, 'ProductTable', {
+      partitionKey: { name: 'id', type: AttributeType.STRING },
+      tableName: 'products',
+    });
+    const stocksTable = new TableV2(this, 'StocksTable', {
+      partitionKey: { name: 'product_id', type: AttributeType.STRING },
+      tableName: 'stocks',
+    });
+
+    const commonLambdaProps = {
       runtime: Runtime.NODEJS_20_X,
+      environment: {
+        PRODUCTS_TABLE_NAME: productsTable.tableName,
+        STOCKS_TABLE_NAME: stocksTable.tableName,
+      },
+    };
+
+    this.getProductsList = new NodejsFunction(this, 'LambdaGetProductsList ', {
+      ...commonLambdaProps,
       handler: 'handler',
       functionName: 'get-products-list',
       entry: 'src/handlers/getProductsList.ts',
     });
     this.getProductsById = new NodejsFunction(this, 'LambdaGetProductsById ', {
-      runtime: Runtime.NODEJS_20_X,
+      ...commonLambdaProps,
       handler: 'handler',
       functionName: 'get-products-by-id',
       entry: 'src/handlers/getProductsById.ts',
     });
+
+    productsTable.grantReadData(this.getProductsList);
+    productsTable.grantReadData(this.getProductsById);
+
+    stocksTable.grantReadData(this.getProductsList);
+    stocksTable.grantReadData(this.getProductsById);
 
     this.api = new apigateway.RestApi(this, 'ProductsRestApi', {
       restApiName: 'products-rest-api',
