86e1ee390717a42497e855cafdc1fa4a2456c6d6
Artem Lobovskii <lobovskiy@yandex.ru>
2024-06-22 12:25:50 +0300

    feat: update tests for get product list handler

--- services/products/test/getProductsList.test.ts
+++ services/products/test/getProductsList.test.ts
@@ -1,29 +1,124 @@
+import {
+  APIGatewayEventRequestContextWithAuthorizer,
+  APIGatewayProxyEvent,
+} from 'aws-lambda';
+import { marshall } from '@aws-sdk/util-dynamodb';
+
+import * as dbService from '../src/database/service';
 import { handler } from '../src/handlers/getProductsList';
 import { responseHeaders } from '../src/handlers/consts';
-import { products } from '../src/__mocks__/products';
+import { DB_TABLE_NAMES } from '../src/consts';
+import { IAvailableProduct } from '../src/types';
+import { products as productsMock } from '../src/__mocks__/products';
+import { stocks as stocksMock } from '../src/__mocks__/stocks';
 
-const isProduct = (obj: unknown) => {
+const isAvailableProduct = (obj: unknown) => {
   return (
     Object.prototype.hasOwnProperty.call(obj, 'id') &&
     Object.prototype.hasOwnProperty.call(obj, 'title') &&
     Object.prototype.hasOwnProperty.call(obj, 'description') &&
-    Object.prototype.hasOwnProperty.call(obj, 'price')
+    Object.prototype.hasOwnProperty.call(obj, 'price') &&
+    Object.prototype.hasOwnProperty.call(obj, 'count')
   );
 };
 
-describe('getProductsList handler', () => {
-  it('should return list of products with status code 200', async () => {
-    const result = await handler();
+const mockApiGatewayProxyEvent: APIGatewayProxyEvent = {
+  body: null,
+  headers: {},
+  multiValueHeaders: {},
+  httpMethod: 'GET',
+  isBase64Encoded: false,
+  path: '',
+  pathParameters: {},
+  queryStringParameters: null,
+  multiValueQueryStringParameters: null,
+  stageVariables: null,
+  requestContext: {} as APIGatewayEventRequestContextWithAuthorizer<object>,
+  resource: '',
+};
 
+const productsTableItemsMock = productsMock.map((product) => marshall(product));
+const stocksTableItemsMock = stocksMock.map((stock) => marshall(stock));
+
+describe('getProductsList handler', () => {
+  const PREV_ENV = process.env;
+  const scanTableMock = jest.spyOn(dbService, 'scanTable');
+
+  beforeEach(() => {
+    jest.resetModules();
+    process.env = { ...PREV_ENV };
+
+    process.env.PRODUCTS_TABLE_NAME = DB_TABLE_NAMES.Products;
+    process.env.STOCKS_TABLE_NAME = DB_TABLE_NAMES.Stocks;
+
+    scanTableMock.mockImplementation((tableName: string) => {
+      if (tableName === DB_TABLE_NAMES.Products) {
+        return Promise.resolve(productsTableItemsMock);
+      }
+
+      if (tableName === DB_TABLE_NAMES.Stocks) {
+        return Promise.resolve(stocksTableItemsMock);
+      }
+
+      return Promise.resolve([]);
+    });
+  });
+
+  afterEach(() => {
+    scanTableMock.mockClear();
+  });
+
+  afterAll(() => {
+    process.env = PREV_ENV;
+  });
+
+  it('should return list of products with status code 200', async () => {
+    const result = await handler(mockApiGatewayProxyEvent);
+
+    expect(scanTableMock).toHaveBeenCalledTimes(2);
     expect(result.statusCode).toEqual(200);
     expect(result.headers).toEqual(responseHeaders);
 
     const body = JSON.parse(result.body);
-    expect(body).toEqual(products);
     expect(Array.isArray(body)).toBe(true);
+    expect(body.length).toEqual(productsMock.length);
+    body.forEach((obj: unknown) => expect(isAvailableProduct(obj)).toBe(true));
+  });
 
-    if (body.length > 0) {
-      body.forEach((obj: unknown) => expect(isProduct(obj)).toBe(true));
-    }
+  it('should return list of products with each product count = 0 if there are no stocks gotten', async () => {
+    scanTableMock.mockImplementation((tableName: string) => {
+      if (tableName === DB_TABLE_NAMES.Products) {
+        return Promise.resolve(productsTableItemsMock);
+      }
+
+      return Promise.resolve([]);
+    });
+
+    const result = await handler(mockApiGatewayProxyEvent);
+
+    expect(scanTableMock).toHaveBeenCalledTimes(2);
+    expect(result.statusCode).toEqual(200);
+    expect(result.headers).toEqual(responseHeaders);
+
+    const body = JSON.parse(result.body);
+    expect(Array.isArray(body)).toBe(true);
+    expect(body.length).toEqual(productsMock.length);
+    body.forEach((product: IAvailableProduct) => expect(product.count).toBe(0));
+  });
+
+  it('should return error with status code 500 if service error occurs', async () => {
+    const error = new Error('Test error');
+    scanTableMock.mockImplementation(() => {
+      throw error;
+    });
+
+    const result = await handler(mockApiGatewayProxyEvent);
+
+    expect(scanTableMock).toHaveBeenCalledTimes(1);
+    expect(result.statusCode).toEqual(500);
+    expect(result.headers).toEqual(responseHeaders);
+
+    const body = JSON.parse(result.body);
+    expect(body.message).toEqual(error.message);
   });
 });
