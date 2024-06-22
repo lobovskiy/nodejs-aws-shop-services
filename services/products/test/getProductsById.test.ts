7f3b0b8ecf84321baabd9c441880b2321d9f0b1b
Artem Lobovskii <lobovskiy@yandex.ru>
2024-06-22 13:30:40 +0300

    feat: update tests for get product by id handler

--- services/products/test/getProductsById.test.ts
+++ services/products/test/getProductsById.test.ts
@@ -1,10 +1,16 @@
-import { handler } from '../src/handlers/getProductsById';
-import { responseHeaders } from '../src/handlers/consts';
-import { products } from '../src/__mocks__/products';
 import {
   APIGatewayEventRequestContextWithAuthorizer,
   APIGatewayProxyEvent,
 } from 'aws-lambda';
+
+import * as dbService from '../src/database/service';
+import { handler } from '../src/handlers/getProductsById';
+import { responseHeaders } from '../src/handlers/consts';
+import { DB_TABLE_NAMES } from '../src/consts';
+import { products as productsMock } from '../src/__mocks__/products';
+import { stocks as stocksMock } from '../src/__mocks__/stocks';
+import { marshall } from '@aws-sdk/util-dynamodb';
+import { IProduct, IStock } from '../src/types';
 
 const createMockApiGatewayProxyEvent = (id?: string): APIGatewayProxyEvent => ({
   body: null,
@@ -21,20 +27,77 @@ const createMockApiGatewayProxyEvent = (id?: string): APIGatewayProxyEvent => ({
   resource: '',
 });
 
-describe('getProductsById handler', () => {
-  it('should return product with status code 200 when id is provided', async () => {
-    const result = await handler(createMockApiGatewayProxyEvent('1'));
+const getAvailableProductMock = (productMock: IProduct) => {
+  const stockMock = stocksMock.find(
+    (stock) => stock.product_id === productMock.id
+  );
+  const count = stockMock?.count || 0;
 
+  return { ...productMock, count };
+};
+
+describe('getProductsById handler', () => {
+  const PREV_ENV = process.env;
+  const queryTableItemByKeyMock = jest.spyOn(dbService, 'queryTableItemByKey');
+
+  beforeEach(() => {
+    jest.resetModules();
+    process.env = { ...PREV_ENV };
+
+    process.env.PRODUCTS_TABLE_NAME = DB_TABLE_NAMES.Products;
+    process.env.STOCKS_TABLE_NAME = DB_TABLE_NAMES.Stocks;
+
+    queryTableItemByKeyMock.mockImplementation(
+      (keyName: string, keyValue: string, tableName: string) => {
+        let result = null;
+
+        if (tableName === DB_TABLE_NAMES.Products) {
+          result = productsMock.find(
+            (product) => product[keyName as keyof IProduct] === keyValue
+          );
+        }
+
+        if (tableName === DB_TABLE_NAMES.Stocks) {
+          result = stocksMock.find(
+            (stock) => stock[keyName as keyof IStock] === keyValue
+          );
+        }
+
+        return result
+          ? Promise.resolve(marshall(result))
+          : Promise.resolve(null);
+      }
+    );
+  });
+
+  afterEach(() => {
+    queryTableItemByKeyMock.mockClear();
+  });
+
+  afterAll(() => {
+    process.env = PREV_ENV;
+  });
+
+  it('should return product with status code 200 when id is provided', async () => {
+    const productMock = productsMock[0];
+
+    const result = await handler(
+      createMockApiGatewayProxyEvent(productMock.id)
+    );
+
+    expect(queryTableItemByKeyMock).toHaveBeenCalledTimes(2);
     expect(result.statusCode).toEqual(200);
     expect(result.headers).toEqual(responseHeaders);
 
+    const availableProductMock = getAvailableProductMock(productMock);
     const body = JSON.parse(result.body);
-    expect(body).toEqual(products.find((product) => product.id === '1'));
+    expect(body).toEqual(availableProductMock);
   });
 
   it('should return 400 status code with error message when id is missing', async () => {
     const result = await handler(createMockApiGatewayProxyEvent());
 
+    expect(queryTableItemByKeyMock).toHaveBeenCalledTimes(0);
     expect(result.statusCode).toEqual(400);
     expect(result.headers).toEqual(responseHeaders);
 
@@ -45,6 +108,7 @@ describe('getProductsById handler', () => {
   it('should return 404 status code with error message when product is not found', async () => {
     const result = await handler(createMockApiGatewayProxyEvent('999'));
 
+    expect(queryTableItemByKeyMock).toHaveBeenCalledTimes(1);
     expect(result.statusCode).toEqual(404);
     expect(result.headers).toEqual(responseHeaders);
 
@@ -54,12 +118,13 @@ describe('getProductsById handler', () => {
 
   it('should return 500 status code with error message on internal server error', async () => {
     const error = new Error('Test error');
-    jest.spyOn(products, 'find').mockImplementationOnce(() => {
+    queryTableItemByKeyMock.mockImplementation(() => {
       throw error;
     });
 
     const result = await handler(createMockApiGatewayProxyEvent('1'));
 
+    expect(queryTableItemByKeyMock).toHaveBeenCalledTimes(1);
     expect(result.statusCode).toEqual(500);
     expect(result.headers).toEqual(responseHeaders);
 
